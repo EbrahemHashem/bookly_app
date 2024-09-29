eb290af55858b4d47c7d0b20dbdf36faa98da7ae
Ebrahem <ebrahemhashem72@gmail.com>
2024-09-29 05:17:06 +0300

    add titile and subtitle to details view

--- lib/features/home/presentation/views/widgets/book details view widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book details view widgets/book_details_view_body.dart
@@ -1,4 +1,6 @@
+import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/book%20details%20view%20widgets/custom_book_details_appbar.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/home%20view%20widgets/custom_list_view_item.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
@@ -6,11 +8,32 @@ class BookDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 30),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
       child: Column(
         children: [
-          CustomBookDetailsAppbar(),
+          const CustomBookDetailsAppbar(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * .15),
+            child: const CustomListViewItem(),
+          ),
+          const SizedBox(
+            height: 40,
+          ),
+          Text(
+            'The Jungle Book',
+            style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(
+            height: 4,
+          ),
+          const Opacity(
+            opacity: .7,
+            child: Text(
+              'Rudyard Kipling',
+              style: Styles.textStyle18,
+            ),
+          ),
         ],
       ),
     );
