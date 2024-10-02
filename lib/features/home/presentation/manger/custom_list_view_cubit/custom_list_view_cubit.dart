5c209b5a64963b65287e032d74762c3762a751ee
Ebrahem <ebrahemhashem72@gmail.com>
2024-10-02 21:11:56 +0300

    create fetch featured books method

--- lib/features/home/presentation/manger/custom_list_view_cubit/custom_list_view_cubit.dart
+++ lib/features/home/presentation/manger/custom_list_view_cubit/custom_list_view_cubit.dart
@@ -1,9 +1,21 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/home/data/models/bookly_model/item.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:equatable/equatable.dart';
 
 part 'custom_list_view_state.dart';
 
 class CustomListViewCubit extends Cubit<CustomListViewState> {
-  CustomListViewCubit() : super(CustomListViewInitial());
+  CustomListViewCubit(this.homeRepo) : super(CustomListViewInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(CustomListViewLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold(
+      (failure) => emit(CustomListViewFailure(errMessage: failure.errMessage)),
+      (books) => emit(CustomListViewSuccess(books: books)),
+    );
+  }
 }
