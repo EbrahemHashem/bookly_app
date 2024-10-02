30eeecd6ed5ac5dc876d8379babc3eea2411f192
Ebrahem <ebrahemhashem72@gmail.com>
2024-10-02 19:54:20 +0300

    create dio server error

--- lib/core/errors/failure.dart
+++ lib/core/errors/failure.dart
@@ -1,3 +1,37 @@
-abstract class Failure {}
+import 'package:dio/dio.dart';
 
-class ServerFailure extends Failure {}
+abstract class Failure {
+  final String errMessage;
+
+  Failure(this.errMessage);
+}
+
+class ServerFailure extends Failure {
+  ServerFailure(super.errMessage);
+  factory ServerFailure.fromDioException(DioException dioException) {
+    switch (dioException.type) {
+      case DioExceptionType.connectionTimeout:
+        return ServerFailure('connection timeout with Api Server');
+      case DioExceptionType.sendTimeout:
+        return ServerFailure('send timeout with Api Server');
+      case DioExceptionType.receiveTimeout:
+        return ServerFailure('receive timeout with Api Server');
+      // response
+      case DioExceptionType.badCertificate:
+        return ServerFailure('bad certificate timeout with Api Server');
+      case DioExceptionType.badResponse:
+        return ServerFailure('bad response  with Api Server');
+      case DioExceptionType.cancel:
+        return ServerFailure('cancel  with Api Server');
+      case DioExceptionType.connectionError:
+        if (dioException.message!.contains('SocketException')) {
+          return ServerFailure('connectionError timeout with Api Server');
+        }
+        return ServerFailure('un expected error with Api Server');
+      case DioExceptionType.unknown:
+        return ServerFailure('unknown  with Api Server');
+      default:
+        return ServerFailure('un expected error with Api Server');
+    }
+  }
+}
--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -3,6 +3,7 @@ import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:bookly_app/features/home/data/models/bookly_model/item.dart';
 import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
 
 class HomeRepoImpl implements HomeRepo {
   final ApiService apiService;
@@ -15,7 +16,10 @@ class HomeRepoImpl implements HomeRepo {
       List<Item> books = [];
       return right(books);
     } catch (e) {
-      return left(ServerFailure());
+      if (e is DioException) {
+        return left(ServerFailure.fromDioException(e));
+      }
+      return left(ServerFailure(e.toString()));
     }
   }
 
